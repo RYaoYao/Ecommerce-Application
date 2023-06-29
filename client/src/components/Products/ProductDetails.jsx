e3e6827d8d65d793e06129156839ea5b9dcc233a
Ryan Christian Yao <58509905+RYaoYao@users.noreply.github.com>
2023-06-29 17:57:58 +0800

    update productdetails.jsx

--- client/src/components/Products/ProductDetails.jsx
+++ client/src/components/Products/ProductDetails.jsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../../styles/styles";
 
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineMessage,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 const ProductDetails = ({ data }) => {
   const [count, setCount] = useState(1);
   const [select, setSelect] = useState(1);
@@ -21,12 +26,12 @@ const ProductDetails = ({ data }) => {
 
   const handleMessageSubmit = () => {
     navigate("/inbox?converstaio=507dasdsaad5v41ds");
-  }
+  };
   return (
     <div className="bg-white">
       {data ? (
         <div
-          className={`unset ${styles.section} w-[90%] 800px:w-[80%] h-screen`}
+          className={`unset ${styles.section} w-[90%] 800px:w-[80%]`}
         >
           <div className="w-full py-5">
             <div className="block w-full 800px:flex">
@@ -47,6 +52,18 @@ const ProductDetails = ({ data }) => {
                       alt=""
                       className="h-[200px]"
                       onClick={() => setSelect(0)}
+                    />
+                  </div>
+                  <div
+                    className={`${
+                      select === 0 ? "border" : "null"
+                    } cursor-pointer`}
+                  >
+                    <img
+                      src={data?.image_Url[1].url}
+                      alt=""
+                      className="h-[200px]"
+                      onClick={() => setSelect(1)}
                     />
                   </div>
                   <div
@@ -113,14 +130,49 @@ const ProductDetails = ({ data }) => {
                       )}
                     </div>
                   </div>
+                  <div
+                    className={`${styles.button} mt-6 !rounded !h-11 flex items-center`}
+                  >
+                    <span className="text-white flex items-center">
+                      Add to card <AiOutlineShoppingCart className="ml-1" />
+                    </span>
+                  </div>
+                  <div className="flex items-center pt-8">
+                    <img src={data.shop.shop_avatar.url} alt="" className="w-[70px] h-[70px] rounded-full mr-2"/>
+                    <div className="">
+                      <h3 className={`${styles.shop_name} pb-1 pt-1 text-[18px]`}>{data.shop.name}</h3>
+                      <h5 className="pb-3 text-[16px]">({data.shop.ratings}) ratings</h5>
+                    </div>
+                    <div className={`${styles.button} bg-[#6443d1] mt-4 !rounded !h-11 ml-5` } onClick={handleMessageSubmit}>
+                      <span className=" text-white flex items-center">Send Message <AiOutlineMessage className="ml-1" /></span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <ProductDetailsInfor data={data} />
         </div>
       ) : null}
     </div>
   );
 };
 
+const ProductDetailsInfor = () => {
+  const [active, setActive] = useState(1)
+    return (
+      <div className="bg-[#f5f6fb] px-3 800px:px10 py-2 rounded">
+        <div className="w-full flex justified-between border-b pt-10 pb-2">
+          <div className="relative">
+            <h5 className="text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]" onClick={() => setActive(1)}>Product Details</h5>
+            {active === 1? (
+              <div className={`${styles.active_indicator}`}>
+
+              </div>
+            ): null}
+          </div>
+        </div>
+      </div>
+    )
+}
 export default ProductDetails;
